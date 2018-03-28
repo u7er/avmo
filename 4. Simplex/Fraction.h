52430b5c82934992e8dfd3fa51be990ee32e98fc
Pavel Manakov <james.swift@yandex.ru>
2018-03-28 12:26:28 +0700

    4 lab

---
+++ 4. Simplex/Fraction.h
@@ -0,0 +1,125 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <cstdlib>
+
+class Fraction {
+public:
+    int m_numerator;
+    int m_denominator;
+    Fraction();
+
+    Fraction(int numerator, int denominator);
+
+    int getNumerator();
+
+    void setNumerator(int numerator);
+
+    int getDenominator();
+
+    void setDenominator(int denominator);
+
+    double convertToDouble();
+
+    int covertToInt();
+
+    friend std::istream &operator>>(std::istream &input, Fraction &fraction);
+
+    friend std::ostream &operator<<(std::ostream &output, const Fraction &fraction);
+
+    const Fraction &operator=(int number);
+
+    Fraction operator-() const;
+
+    const Fraction operator+(const Fraction &fraction) const;
+
+    const Fraction operator-(const Fraction &fraction) const;
+
+    const Fraction operator*(const Fraction &fraction) const;
+
+    const Fraction operator/(const Fraction &fraction) const;
+
+    const Fraction operator+(int number) const;
+
+    const Fraction operator-(const int number) const;
+
+    const Fraction operator*(const int number) const;
+
+    const Fraction operator/(const int number) const;
+
+    const friend Fraction operator+(const int &number, const Fraction &fraction);
+
+    const friend Fraction operator-(const int &number, const Fraction &fraction);
+
+    const friend Fraction operator*(const int &number, const Fraction &fraction);
+
+    const friend Fraction operator/(const int &number, const Fraction &fraction);
+
+    Fraction &operator++();
+
+    Fraction operator++(const int);
+
+    Fraction &operator--();
+
+    Fraction operator--(const int);
+
+    const Fraction &operator+=(const int number);
+
+    const Fraction &operator-=(const int number);
+
+    const Fraction &operator*=(const int number);
+
+    const Fraction &operator/=(const int number);
+
+    const Fraction &operator+=(const Fraction &fraction);
+
+    const Fraction &operator-=(const Fraction &fraction);
+
+    const Fraction &operator*=(const Fraction &fraction);
+
+    const Fraction &operator/=(const Fraction &fraction);
+
+    const bool operator>(const Fraction &fraction) const;
+
+    const bool operator>=(const Fraction &fraction) const;
+
+    const bool operator<(const Fraction &fraction) const;
+
+    const bool operator<=(const Fraction &fraction) const;
+
+    const bool operator==(const Fraction &fraction) const;
+
+    const bool operator!=(const Fraction &fraction) const;
+
+    friend const bool operator>(const int &number, const Fraction &fraction);
+
+    friend const bool operator>=(const int &number, const Fraction &fraction);
+
+    friend const bool operator<(const int &number, const Fraction &fraction);
+
+    friend const bool operator<=(const int &number, const Fraction &fraction);
+
+    friend const bool operator==(const int &number, const Fraction &fraction);
+
+    friend const bool operator!=(const int &number, const Fraction &fraction);
+
+    friend const bool operator>(const double &number, const Fraction &fraction);
+
+    friend const bool operator>=(const double &number, const Fraction &fraction);
+
+    friend const bool operator<(const double &number, const Fraction &fraction);
+
+    friend const bool operator<=(const double &number, const Fraction &fraction);
+
+    friend const bool operator==(const double &number, const Fraction &fraction);
+
+    friend const bool operator!=(const double &number, const Fraction &fraction);
+
+    const Fraction operator^(int n) const;
+
+    ~Fraction();
+
+private:
+    int gcd(int mumber1, int number2);
+
+};
